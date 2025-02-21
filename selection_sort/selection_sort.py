5cdd4dc8e57effd59866a96420151b55fbdbad67
Ricardo dAvila <ricardo.davila.freitas@gmail.com>
2025-02-21 08:58:39 -0300

    Update Selection Sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -4,28 +4,27 @@ Github: https://github.com/ricdavila
 """
 
 def ordenar_lista(lista):
-    """Ordena a lista dada com o algoritmo de Ordenação por Seleção — Complexidade O(n²)."""
 
-    lista_ordenada = []
-    # para cada elemento da lista, encontra o menor valor e o adiciona à lista ordenada
-    for i in range(len(lista)):
-        menor = buscar_menor(lista) # encontra o menor valor no restante da lista
-        lista_ordenada.append(lista.pop(menor)) # adiciona o valor à nova lista e o remove da lista antiga
-    
-    return lista_ordenada # retorna a nova lista
+    n = len(lista) #tamanho da lista
 
+    if n == 1:
+        return lista
 
-def buscar_menor(lista):
-    """Algoritmo para encontrar o indíce do menor elemento da lista."""
+    # loop externo
+    for i in range(n-1):
 
-    menor = 0
+        menor = i
 
-    for i in range(len(lista)):
-        # atualiza o valor do menor índice caso necessário
-        if lista[i] < lista[menor]:
-            menor = i
+        # loop interno
+        for j in range(i + 1, n):
+            if lista[j] < lista [menor]:
+                menor = j
 
-    return menor # retorna o índice do menor valor encontrado
+        # reposiciona o menor valor
+        if menor != i:
+            lista[i], lista[menor] = lista[menor], lista[i]
+
+    return lista # retorna a lista ordenada
 
 
 if __name__ == '__main__':
